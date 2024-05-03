0664b28cb882eb29da73d1926e6ffe8fdc6e0424
prathamaarafi <rafiprathama27@student.uns.ac.id>
2024-05-03 10:21:22 +0700

    soal4

--- src/app/Soal4/page.jsx
+++ src/app/Soal4/page.jsx
@@ -1,5 +1,6 @@
 "use client";
 import Navbar from "@/components/navbar";
+import { Button } from "@/components/ui/button";
 import React from "react";
 
 function Soal4Page() {
@@ -10,8 +11,47 @@ function Soal4Page() {
         <Navbar></Navbar>
       </nav>
       <main className="flex flex-wrap justify-center mt-20 mx-10 ">
-        <div>
-          <p>Soal 4</p>
+        <div className="flex flex-row gap-4 mb-7">
+          <Button
+            className="
+            hover:bg-purple-100"
+            variant="secondary"
+          >Generate Random DFA</Button>
+          <Button
+            className="
+            hover:bg-purple-100"
+            variant="secondary"
+          >Generate Random NFA</Button>
+          <Button
+            className="
+            hover:bg-purple-100"
+            variant="secondary"
+          >Generate Random eNFA</Button>
+        </div>
+        <div className="grid w-full max-w-sm items-center gap-1.5">
+          <Label htmlFor="many_state">States (gunakan spasi)</Label>
+          <Input id="many_state" placeholder="Banyak State" />
+        </div>
+        <div className="grid w-full max-w-sm items-center gap-1.5 mt-7">
+          <Label htmlFor="alphabet">Alphabet (gunakan spasi)</Label>
+          <Input id="alphabet" placeholder="Simbol" />
+        </div>
+        <div className="grid w-full max-w-sm items-center gap-1.5 mt-7">
+          <Label htmlFor="initial_state">Initial State (hanya 1)</Label>
+          <Input id="initial_state" placeholder="State Pertama" />
+        </div>
+        <div className="grid w-full max-w-sm items-center gap-1.5 mt-7">
+          <Label htmlFor="final_state">Final State (gunakan spasi)</Label>
+          <Input id="final_state" placeholder="State yang diterima" />
+        </div>
+        <div className="grid w-full max-w-md mt-7">
+          <Button
+            className="bg-purple-500 text-white 
+            hover:bg-purple-800"
+            variant="secondary"
+          >
+            Make Graph
+          </Button>
         </div>
       </main>
     </>
